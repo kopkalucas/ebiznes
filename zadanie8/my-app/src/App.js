833efea728fc28e46d6ff95866915a3146da9080
kopkalucas <kopka.lucas@gmail.com>
2024-08-27 19:52:47 +0200

    zadanie8

---
+++ zadanie8/my-app/src/App.js
@@ -0,0 +1,47 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './home';
+import Login from './login';
+import './App.css';
+import { useEffect, useState } from 'react';
+
+function App() {
+  const [loggedIn, setLoggedIn] = useState(false)
+  const [email, setEmail] = useState("")
+
+  useEffect(() => {
+   
+    const user = JSON.parse(localStorage.getItem("user"))
+
+    
+    if (!user || !user.token) {
+      setLoggedIn(false)
+      return
+    }
+
+    
+    fetch("http://localhost:3080/verify", {
+            method: "POST",
+            headers: {
+                'jwt-token': user.token
+              }
+        })
+        .then(r => r.json())
+        .then(r => {
+            setLoggedIn('success' === r.message)
+            setEmail(user.email || "")
+        })
+  }, [])
+
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home email={email} loggedIn={loggedIn} setLoggedIn={setLoggedIn}/>} />
+          <Route path="/login" element={<Login setLoggedIn={setLoggedIn} setEmail={setEmail} />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
